536762499f06a1a867ac9ad822bfdf8682038524
Sahil Raina <sahilraina2004.sr@gmail.com>
2024-03-05 00:36:20 +0530

    added sample

---
+++ java/sample.java
@@ -0,0 +1,11 @@
+class ABS{
+
+    public void Hello1(){
+        System.out.println("Hello World");
+        
+    }
+    public static void main(String[] args) {
+        ABS A = new ABS();
+        A.Hello1();
+    }
+}
