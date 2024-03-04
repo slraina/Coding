0f1f0fcf4ee0e2924f1611f2d816340d2f547e66
Sahil Raina <sahilraina2004.sr@gmail.com>
2024-03-05 00:46:43 +0530

    ABC Class Deleted

--- java/sample.java
+++ java/sample.java
@@ -1,7 +1,7 @@
 class ABS{
 
     public void Hello1(){
-        System.out.println("Hello World");
+        System.out.println("Hello   World");
         
     }
     public static void main(String[] args) {
